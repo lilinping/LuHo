bc61fb24593d73556f2dfc53f2efe1815979727d
lilinping <lilinping>
2020-12-03 18:08:11 +0800

    hello LuHo

---
+++ LuHo.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: LLP
+ * @Date: 2020-12-03 18:03:09
+ * @LastEditTime: 2020-12-03 18:03:37
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \LuHo\LuHo.js
+ */
+
+console.log('hello LuHo')
